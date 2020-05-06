3c5a80f6708f5164f7747bf74b9518984c015000
block-cat <kfx2007@outlook.com>
2020-05-06 08:02:09 +0000

    description

--- __manifest__.py
+++ __manifest__.py
@@ -7,8 +7,8 @@
     'description': """
         支付宝集成支付
     """,
-    'author': "black-cat",
-    'website': "http://mixoo.cn",
+    'author': "Kevin Kong",
+    'website': "http://www.odoomommy.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
